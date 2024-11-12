ce49cb38bdccb2f42b29ae02883dc127e658353d
haru <haru.lim14@gmail.com>
2024-11-12 01:14:59 -0300

    update13

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node backend/server.mjs
+web: npm start
--- backend/jest.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-    testEnvironment: 'node', // Definindo o ambiente de teste
-    transform: {
-      '^.+\\.mjs$': 'babel-jest', // Se você estiver usando Babel, adicione essa linha
-    },
-    moduleFileExtensions: ['js', 'mjs', 'json', 'node'], // Adicionando extensões que o Jest deve reconhecer
-  };
-  
